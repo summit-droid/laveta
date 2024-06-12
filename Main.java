913b348a6d632d37c4d6d813fed319fb78df5458
summit-droid <mwangidouglas446@gmail.com>
2024-06-12 13:22:38 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Rectangle {
+  void Area(double L, double W){
+  System.out.println(L*W);
+  }
+
+public class Main{
+  public static void main(String[] args){
+      rectangle Obj = new rectangle();
+      Obj.Area(20,15);
+  }
+}
+}
